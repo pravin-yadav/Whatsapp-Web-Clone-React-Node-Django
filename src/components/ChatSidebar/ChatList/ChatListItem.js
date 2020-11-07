a9866e3a5607abfe4666a32be00e24be536d4776
Pravin Yadav <work.coderpravin@gmail.com>
2020-11-07 21:03:37 +0530

    [Whatsapp Web] Added Chatlist component and its style scss on sidebar.

--- src/components/ChatSidebar/ChatList/ChatListItem.js
+++ src/components/ChatSidebar/ChatList/ChatListItem.js
@@ -3,7 +3,20 @@ import React from 'react';
 class ChatListItem extends React.Component {
     render() {
         return (
-            <h1>Pravin Yadav</h1>
+            <div className="chat-sidebar-chatlist--listitem">
+                <img className="chat-sidebar-chatlist--avatar" src="/assets/images/profile_pic.jpg" alt="" />
+                <div className="chat-sidebar-chatlist--content">
+                    <div className="chat-sidebar-chatlist--data">
+                        <div className="chat-sidebar-chatlist--name">Pravin Yadav </div>
+                        <div className="chat-sidebar-chatlist--time">Thu 19:20</div>
+                    </div>
+                    <div className="chat-sidebar-chatlist--data">
+                        <div className="chat-sidebar-chatlist--text">
+                            Hey,How are you Hey,How are you Hey,How are you Hey,How are you Hey,How are you Hey,How are you Hey,How are you?
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
--- src/components/ChatSidebar/ChatList/index.js
+++ src/components/ChatSidebar/ChatList/index.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import ChatListItem from './ChatListItem';
 
 export const ChatList = () => {
-    const [chatList] = useState([{}, {}, {}, {}])
+    const [chatList] = useState([{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}])
     return (
-        <div>
+        <div className="chat-sidebar--chatlist">
             {chatList.map((chat, index) => <ChatListItem key={index} data={chat} />)}
         </div>
     )
